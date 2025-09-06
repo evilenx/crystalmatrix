0451a86730648cb8f71a7033ae8fda8e8e74188f
evilenx <evilenx@proton.me>
2025-09-06 02:04:16 -0500

    screen full and fps 144

--- src/rg0x.cr
+++ src/rg0x.cr
@@ -1,26 +1,48 @@
 require "random"
 
-# Paleta de grises en colores ANSI 256 (oscuro a claro)
 GRAY_SCALE = [232, 235, 237, 240, 245, 250, 254, 255]
-
-# Parámetros de tamaño usando variables de entorno del terminal
-width  = (ENV["COLUMNS"]? || "80").to_i
-height = (ENV["LINES"]? || "24").to_i
-
-# Caracteres Matrix
 CHARS = ('0'..'1').to_a
 
-loop do
-  (0...height).each do
-    row = ""
-    width.times do
-      char = CHARS.sample
-      gray = GRAY_SCALE.sample
-      row += "\e[38;5;#{gray}m#{char}\e[0m"
+# Cambia aquí los FPS que desees (ej: 144 para muy fluido, 30 para efecto lento)
+FPS = 144
+FRAME_TIME = 1.0 / FPS.to_f
+
+def term_size
+  width  = (ENV["COLUMNS"]? || `tput cols`.strip).to_i
+  height = (ENV["LINES"]?   || `tput lines`.strip).to_i
+  {width, height}
+end
+
+def clear_screen
+  print "\e[2J"
+  print "\e[H"
+end
+
+def hide_cursor
+  print "\e[?25l"
+end
+
+def show_cursor
+  print "\e[?25h"
+end
+
+begin
+  hide_cursor
+  loop do
+    width, height = term_size
+    clear_screen
+    height.times do
+      row = ""
+      width.times do
+        char = CHARS.sample
+        gray = GRAY_SCALE.sample
+        row += "\e[38;5;#{gray}m#{char}\e[0m"
+      end
+      puts row
     end
-    puts row
+    sleep FRAME_TIME.seconds
   end
-  sleep 0.03.seconds
-  print "\e[#{height}A" # Subir el cursor
+ensure
+  show_cursor
 end
 
